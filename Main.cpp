8e3fed4a00d93616e06c8ca46d6f59b568c1456a
STMTP <168919222+STMTP@users.noreply.github.com>
2024-06-28 17:45:02 +0700

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <sstream>
+#include "prim.h"
+
+int main() {
+    Graph g;
+    g.readFromFile("input.txt");
+
+    // Thuật toán Prim tìm cây khung cực tiểu của đồ thị
+    std::cout << "Cay khung cuc tieu cua do thi:" << std::endl;
+    g.prim();
+
+    // Người dùng nhập cạnh (x, y) và tìm cây khung cực tiểu chứa cạnh đó
+    int x, y;
+    std::string input;
+    std::cout << "Nhap canh (x, y) bao gom trong cay khung (VD: 5, 6): ";
+
+    while (true) {
+        std::getline(std::cin, input); // Lưu vào biến input
+        std::stringstream ss(input); // Đọc dữ liệu từ chuỗi input
+
+        // Kiểm tra và tách giá trị x, y từ chuỗi
+        if (ss >> x && ss.get() == ',' && ss >> y) // Đọc giá trị đầu vào x & y, kiểm tra dấu ','
+        {
+            break; // Đầu vào hợp lệ
+        }
+        else // Đầu vào không hợp lệ
+        {
+            std::cout << "Dinh dang khong hop le. Vui long nhap lai canh (x, y) (VD: 5, 6): ";
+        }
+    }
+    g.primWithEdge(x, y);
+    return 0;
+}
